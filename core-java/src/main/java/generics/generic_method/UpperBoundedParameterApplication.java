1887a99e423dd3a1c33068bd86c1cb29d25b2903
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2024-01-20 14:04:20 +0200

    adding Generic class with Upper bounded type

---
+++ core-java/src/main/java/generics/generic_method/UpperBoundedParameterApplication.java
@@ -0,0 +1,25 @@
+package generics.generic_method;
+
+import java.util.List;
+
+public class BoundedParameterApplication {
+    public static void main(String[] args) {
+        AverageCalculator<Integer> averageCalculator = new AverageCalculator<>();
+        averageCalculator.setNumbers(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        System.out.println(averageCalculator.calculateAverage());
+    }
+}
+
+class AverageCalculator<T extends Number> {
+    private List<T> numbers;
+
+    public void setNumbers(List<T> numbers) {
+        this.numbers = numbers;
+    }
+
+    public double calculateAverage(){
+        return this.numbers.stream()
+                .mapToDouble(Number::doubleValue)
+                .average().getAsDouble();
+    }
+}
