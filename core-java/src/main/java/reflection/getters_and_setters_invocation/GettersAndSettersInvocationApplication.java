be7299ffe46357ab959606da0e4d85e7e3de115c
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2024-01-25 18:35:24 +0200

    generating Getters & Setters with reflection

--- core-java/src/main/java/reflection/getters_and_setters_invocation/GettersAndSettersInvocationApplication.java
+++ core-java/src/main/java/reflection/getters_and_setters_invocation/GettersAndSettersInvocationApplication.java
@@ -1,7 +1,49 @@
 package reflection.getters_and_setters_invocation;
 
+import java.lang.reflect.Field;
+
 public class GettersAndSettersInvocationApplication {
     public static void main(String[] args) {
+        try{
+            Class clazz = Class.forName("reflection.getters_and_setters_invocation.Person");
+            Field[] declaredFields = clazz.getDeclaredFields();
+            StringBuffer stringBuffer = new StringBuffer();
+            for(Field field : declaredFields){
+                String fieldName = field.getName();
+                String fieldType = field.getType().getSimpleName();
+                CommonUtils.createSetter(fieldName, fieldType, stringBuffer);
+                CommonUtils.createGetter(fieldName, fieldType, stringBuffer);
+            }
 
+            System.out.println(stringBuffer.toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    private boolean vegetarian;
+}
+
+class CommonUtils {
+
+    public static void createSetter(String fieldName, String fieldType, StringBuffer stringBuffer) {
+        String setterMethod = String.format("public void set%s(%s %s){this.%s = %s;}", camelCase(fieldName), fieldType, fieldName, fieldName, fieldName);
+        stringBuffer.append(setterMethod);
+        stringBuffer.append("\n");
+
+    }
+
+    public static void createGetter(String fieldName, String fieldType, StringBuffer stringBuffer) {
+        String getterMethod = String.format("public %s get%s(){return this.%s;}",fieldType, camelCase(fieldName), fieldName);
+        stringBuffer.append(getterMethod);
+        stringBuffer.append("\n");
+    }
+
+    public static String camelCase(String message){
+        return message.substring(0, 1).toUpperCase() + message.substring(1);
     }
 }
