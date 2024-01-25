350da6ffdfe97196e3c28faace1d57bfc464b6d5
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2024-01-25 14:23:41 +0200

    adding Reflection constructor invocation

---
+++ core-java/src/main/java/reflection/constructor_invocation/ConstructorInvocationApplication.java
@@ -0,0 +1,56 @@
+package reflection.constructor_invocation;
+
+import lombok.*;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
+public class ConstructorInvocationApplication {
+    public static void main(String[] args) {
+        try{
+            Class clazz = Class.forName("reflection.constructor_invocation.Model");
+
+            System.out.println("-- Getting Constructor with 2 args --");
+            Constructor twoArgsConstructor = clazz.getConstructor(String.class, int.class);
+            System.out.println(twoArgsConstructor);
+            System.out.println();
+
+            System.out.println("-- Getting all public Constructors of the class --");
+            Constructor[] publicConstructors = clazz.getConstructors();
+            System.out.println(Arrays.toString(publicConstructors));
+            System.out.println();
+
+            System.out.println("-- Getting private Constructor of the class --");
+            Constructor privateConstructor = clazz.getDeclaredConstructor();
+            System.out.println(privateConstructor);
+            System.out.println();
+
+            System.out.println("-- Getting all declared Constructors of the class --");
+            Constructor[] declaredConstructors = clazz.getDeclaredConstructors();
+            System.out.println(Arrays.toString(declaredConstructors));
+            System.out.println();
+
+            System.out.println("-- Getting Not-Found Constructor with 2 args --");
+            Constructor notFoundConstructor = clazz.getConstructor(Integer.class, String.class);
+            System.out.println(notFoundConstructor);
+            System.out.println();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+@Setter
+class Model {
+    private String key;
+    private int value;
+
+    public void print(){
+        System.out.println(String.format("Key: %s, Value: %s", key, value));
+    }
+}
