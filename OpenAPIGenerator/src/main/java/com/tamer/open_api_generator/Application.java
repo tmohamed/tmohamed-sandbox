25270f3e280632be1ad6657fdb97074c3810e7fd
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2024-09-14 19:48:19 +0300

    adding demo for Openapi Generator maven plugin

---
+++ OpenAPIGenerator/src/main/java/com/tamer/open_api_generator/Application.java
@@ -0,0 +1,11 @@
+package com.tamer.open_api_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
