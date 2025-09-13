8542f2939e6c9ff4e498bc2d585d2103fd7bfcb0
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2025-09-13 14:07:27 +0300

    Implementing a Resource Server

---
+++ spring-resource-server/src/main/java/com/tamer/spring_resource_server/SpringResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.spring_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceServerApplication.class, args);
+	}
+
+}
---
+++ spring-resource-server/src/main/java/com/tamer/spring_resource_server/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.tamer.spring_resource_server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${keySetURI}")
+    private String keySetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.oauth2ResourceServer(
+                c-> c.jwt(
+                        j->j.jwkSetUri(keySetUri)));
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated());
+
+        return http.build();
+    }
+
+}
