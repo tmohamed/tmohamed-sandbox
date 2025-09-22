7a333af7f9ad73ceba6bb1c918cf94ba840afb24
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2025-09-22 23:18:37 +0300

    Demo application for implementing both UserDetails and UserDetailsService of Spring security

---
+++ spring-security-in-action/ch3-managing-users-sandbox/src/main/java/com/mohamed/tamer/ch3_managing_users_sandbox/Ch3ManagingUsersSandboxApplication.java
@@ -0,0 +1,11 @@
+package com.mohamed.tamer.ch3_managing_users_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3ManagingUsersSandboxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3ManagingUsersSandboxApplication.class, args);
+    }
+}
---
+++ spring-security-in-action/ch3-managing-users-sandbox/src/main/java/com/mohamed/tamer/ch3_managing_users_sandbox/model/User.java
@@ -0,0 +1,34 @@
+package com.mohamed.tamer.ch3_managing_users_sandbox.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+    private final String username;
+    private final String password;
+    private final String authority;
+
+    public User(String username, String password, String authority){
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> this.authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}
