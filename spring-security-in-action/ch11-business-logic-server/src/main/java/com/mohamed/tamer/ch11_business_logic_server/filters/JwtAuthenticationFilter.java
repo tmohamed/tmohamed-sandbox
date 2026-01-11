fb26fe77c761a47217ac9e272b0b632ec4edcec4
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2026-01-11 15:53:28 +0200

    Implementing both Username/Password/Otp Authentication Filter, and Jwt Authentication Filter

---
+++ spring-security-in-action/ch11-business-logic-server/src/main/java/com/mohamed/tamer/ch11_business_logic_server/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.mohamed.tamer.ch11_business_logic_server.filters;
+
+import com.mohamed.tamer.ch11_business_logic_server.authentication_types.UsernamePasswordAuthentication;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.signing.key}")
+    private String signingKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader("Authorization");
+        SecretKey key = Keys.hmacShaKeyFor(signingKey.getBytes(StandardCharsets.UTF_8));
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+
+        String username = claims.get("username", String.class);
+
+        GrantedAuthority a = new SimpleGrantedAuthority("user");
+        var auth = new UsernamePasswordAuthentication(username, null, List.of(a));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().equals("/login");
+    }
+}
