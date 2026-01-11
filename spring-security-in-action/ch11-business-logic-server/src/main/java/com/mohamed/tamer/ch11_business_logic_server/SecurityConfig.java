a23f0d76428dafd7a718addf1f0bac132a1096af
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2026-01-11 16:36:09 +0200

    Adding security configuration for Custom Filters and Custom Authentication Providers

--- spring-security-in-action/ch11-business-logic-server/src/main/java/com/mohamed/tamer/ch11_business_logic_server/SecurityConfig.java
+++ spring-security-in-action/ch11-business-logic-server/src/main/java/com/mohamed/tamer/ch11_business_logic_server/SecurityConfig.java
@@ -1,18 +1,45 @@
 package com.mohamed.tamer.ch11_business_logic_server;
 
+import com.mohamed.tamer.ch11_business_logic_server.authentication_providers.OtpAuthenticationProvider;
+import com.mohamed.tamer.ch11_business_logic_server.authentication_providers.UsernamePasswordAuthenticationProvider;
+import com.mohamed.tamer.ch11_business_logic_server.filters.InitialAuthenticationFilter;
+import com.mohamed.tamer.ch11_business_logic_server.filters.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final InitialAuthenticationFilter initialAuthenticationFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider;
+    private final OtpAuthenticationProvider otpAuthenticationProvider;
+
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+        http.authenticationManager(authenticationManager());
+
+        http.addFilterAt(initialAuthenticationFilter, BasicAuthenticationFilter.class);
+        http.addFilterAfter(jwtAuthenticationFilter, BasicAuthenticationFilter.class);
+
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        return new ProviderManager(usernamePasswordAuthenticationProvider, otpAuthenticationProvider);
     }
 
     @Bean
