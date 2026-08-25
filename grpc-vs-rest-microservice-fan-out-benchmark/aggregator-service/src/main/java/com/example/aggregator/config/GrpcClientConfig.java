3be1eea27707b12117a746bb6f611daab3d90880
Tamer Mohamed <EngTamerMohamedAbdallah@gmail.com>
2026-08-25 15:58:53 +0300

    Update gRPC client configuration in application.yml and GrpcClientConfig for improved service address handling

--- grpc-vs-rest-microservice-fan-out-benchmark/aggregator-service/src/main/java/com/example/aggregator/config/GrpcClientConfig.java
+++ grpc-vs-rest-microservice-fan-out-benchmark/aggregator-service/src/main/java/com/example/aggregator/config/GrpcClientConfig.java
@@ -1,7 +1,6 @@
 package com.example.aggregator.config;
 
 import io.grpc.Channel;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
@@ -9,21 +8,15 @@ import org.springframework.grpc.client.GrpcChannelFactory;
 @Configuration
 public class GrpcClientConfig {
 
-    @Value("${grpc.client.channels.service-a.address}")
-    private String serviceAHost;
-
-    @Value("${grpc.client.channels.service-b.address}")
-    private String serviceBHost;
-
     @Bean
     public com.example.servicea.grpc.generated.DataServiceGrpc.DataServiceBlockingStub serviceABlockingStub(GrpcChannelFactory channelFactory){
-        Channel channel = channelFactory.createChannel(serviceAHost);
+        Channel channel = channelFactory.createChannel("servicea");
         return  com.example.servicea.grpc.generated.DataServiceGrpc.newBlockingStub(channel);
     }
 
     @Bean
     public com.example.serviceb.grpc.generated.DataServiceGrpc.DataServiceBlockingStub serviceBBlockingStub(GrpcChannelFactory channelFactory){
-        Channel channel = channelFactory.createChannel(serviceBHost);
+        Channel channel = channelFactory.createChannel("serviceb");
         return  com.example.serviceb.grpc.generated.DataServiceGrpc.newBlockingStub(channel);
     }
 }
